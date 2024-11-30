fba3f02535cf7166067bb4e86aba8462f13bc0b3
Yulian <105125828+YulianGuinand@users.noreply.github.com>
2024-11-30 19:41:10 +0100

    install editor and first style

---
+++ src/app/documents/[documentId]/page.tsx
@@ -0,0 +1,17 @@
+import { Editor } from "./editor";
+
+interface DocumentIdPageProps {
+  params: Promise<{ documentId: string }>;
+}
+
+const DocumentId = async ({ params }: DocumentIdPageProps) => {
+  const { documentId } = await params;
+
+  return (
+    <div className="min-h-screen bg-[#FAFBFD]">
+      <Editor />
+    </div>
+  );
+};
+
+export default DocumentId;
