206d309912cc4d818ce84867557dae7e04c056d5
Yulian <58071866+RectoVersoDev@users.noreply.github.com>
2024-12-05 10:03:48 +0100

    Feat(Search): add params on url

--- src/app/(home)/search-input.tsx
+++ src/app/(home)/search-input.tsx
@@ -1,11 +1,13 @@
 "use client";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { useSearchParam } from "@/hooks/use-search-param";
 import { SearchIcon, XIcon } from "lucide-react";
 import { useRef, useState } from "react";
 
 export const SearchInput = () => {
-  const [value, setValue] = useState("");
+  const [search, setSearch] = useSearchParam();
+  const [value, setValue] = useState(search);
 
   const inputRef = useRef<HTMLInputElement>(null);
 
@@ -15,12 +17,19 @@ export const SearchInput = () => {
 
   const handleClear = () => {
     setValue("");
+    setSearch("");
+    inputRef.current?.blur();
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSearch(value);
     inputRef.current?.blur();
   };
 
   return (
     <div className="flex-1 flex items-center justify-center">
-      <form className="relative max-w-[720px] h-full">
+      <form className="relative max-w-[720px] h-full" onSubmit={handleSubmit}>
         <Input
           placeholder="Search"
           className="md:text-base placeholder:text-neutral-800 px-14 w-full border-none focus-visible:shadow-[0_1px_1px_0_rgba(65,69,73,.3),0_1px_3px_1px_rgba(65,69,73,.15)] bg-[#F0F4F8] rounded-full h-[48px] focus-visible:ring-0 focus:bg-white"
