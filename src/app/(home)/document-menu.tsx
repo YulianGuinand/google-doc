1ceded7387fab8c127427008e59d2ba6b8ed0c7b
Yulian <58071866+RectoVersoDev@users.noreply.github.com>
2024-12-06 16:05:01 +0100

    Feat(DocumentsList): add table and convex functions

---
+++ src/app/(home)/document-menu.tsx
@@ -0,0 +1,48 @@
+import { RemoveDialog } from "@/components/remove-dialog";
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { ExternalLinkIcon, MoreVertical, TrashIcon } from "lucide-react";
+import { Id } from "../../../convex/_generated/dataModel";
+
+interface DocumentMenuProps {
+  documentId: Id<"documents">;
+  title: string;
+  onNewTab: (id: Id<"documents">) => void;
+}
+
+export const DocumentMenu = ({
+  documentId,
+  title,
+  onNewTab,
+}: DocumentMenuProps) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="ghost" size="icon" className="rounded-full">
+          <MoreVertical />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem onClick={() => onNewTab(documentId)}>
+          <ExternalLinkIcon className="size-4 mr-2" />
+          Open in a new tab
+        </DropdownMenuItem>
+
+        <RemoveDialog documentId={documentId}>
+          <DropdownMenuItem
+            onSelect={(e) => e.preventDefault()}
+            onClick={(e) => e.stopPropagation()}
+          >
+            <TrashIcon className="size-4 mr-2" />
+            Remove
+          </DropdownMenuItem>
+        </RemoveDialog>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  );
+};
